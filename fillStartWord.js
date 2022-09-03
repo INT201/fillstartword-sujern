ae21f43a908d16d626bee8da67eef735ae1e30d5
sujern <sujern7856@gmail.com>
2022-09-03 22:36:16 +0700

    sent HW01

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,8 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  if(word.includes(startWord)){
+    return word
+}
 }
 module.exports = fillStartWord
